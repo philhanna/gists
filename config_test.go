1da7db337c6d2bc5b77f6925c2c9fad933f403dd
Phil Hanna <ph1204@gmail.com>
2024-01-29 19:51:44 -0500

    Added framework of unit tests for LoadConfig

--- config_test.go
+++ config_test.go
@@ -9,3 +9,25 @@ func TestGetConfigFileName(t *testing.T) {
 	filename := GetConfigFileName()
 	assert.NotEmpty(t, filename)
 }
+
+func TestLoadConfig(t *testing.T) {
+	tests := []struct {
+		name         string
+		filename     string
+		wantErr      bool
+		wantUsername string
+		wantToken    string
+	}{}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			config, err := LoadConfig(tt.filename)
+			if tt.wantErr {
+				assert.NotNil(t, err)
+				return
+			}
+			assert.Nil(t, err)
+			assert.Equal(t, tt.wantUsername, config.Username)
+			assert.Equal(t, tt.wantToken, config.Token)
+		})
+	}
+}
