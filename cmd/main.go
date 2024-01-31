7e22418c14ca1096486a3f987bd0982c8f45ea5c
Phil Hanna <ph1204@gmail.com>
2024-01-30 20:09:33 -0500

    Collected sample data

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "gists"
+import (
+	"fmt"
+	"gists"
+	"log"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	gists.GetGists()
+	pageno := 0
+	for gistpage := range gists.GetGistPages() {
+		pageno++
+		filename := filepath.Join("testdata", fmt.Sprintf("gistpage%d.json", pageno))
+		err := writePage(filename, gistpage)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Wrote page %d\n", pageno)
+	}
+}
+
+func writePage(filename string, gistpage string) error {
+	err := os.WriteFile(filename, []byte(gistpage), 0644)
+	return err
 }
--- gist_reader.go
+++ gist_reader.go
@@ -8,48 +8,85 @@ import (
 )
 
 const (
-	URL_PREFIX = "https://api.github.com/users"
-	URL_SUFFIX = "gists?per_page=10"
+	URL_PREFIX       = "https://api.github.com/users"
+	URL_SUFFIX       = "gists?per_page=10"
+	LINK_HEADER_NAME = "Link"
+	REL_NEXT_NAME    = "next"
 )
 
-func GetGists() {
-	// Load the configuration
-	config, err := LoadConfig(GetConfigFileName())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Create the request
-	url := fmt.Sprintf("%s/%s/%s", URL_PREFIX, config.Username, URL_SUFFIX)
-	client := new(http.Client)
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+// AddHeaders adds the required authorization headers to the HTTP
+// request.
+func AddHeaders(req *http.Request) {
+	bearerToken := fmt.Sprintf("Bearer %s", Config.Token)
 	req.Header.Set("Accept", "application/vnd.github+json")
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", config.Token))
+	req.Header.Set("Authorization", bearerToken)
 	req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
+}
 
-	// Make the http request
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
+// GetGistPages downloads pages of gists from GitHub, sending them back as
+// strings of JSON arrays.
+func GetGistPages() chan string {
+	ch := make(chan string, 10)
+	go func() {
+		defer close(ch)
 
-	// Get the body
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+		// Create the URL for the initial request
+		url := MakeInitialURL()
 
-	// Print it
-	fmt.Println(string(body))
+		// Get all pages, starting with this URL
+		for url != "" {
+			client := new(http.Client)
+			req, err := http.NewRequest(http.MethodGet, url, nil)
+			if err != nil {
+				log.Fatal(err)
+			}
+			AddHeaders(req)
 
-	// Get the links
-	for key, values := range resp.Header {
-		fmt.Println("Header:", key)
-		for _, value := range values {
-			fmt.Println("Value:", value)
+			// Make the request
+			resp, err := client.Do(req)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Get the body
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Send it through the output channel
+			ch <- string(body)
+
+			// Get the URL for the next page (or blank, if there are no
+			// more)
+			url = MakeNextURL(resp)
+		}
+	}()
+	return ch
+
+}
+
+// MakeInitialURL creates the URL for the first page of the gists
+func MakeInitialURL() string {
+	url := fmt.Sprintf("%s/%s/%s", URL_PREFIX, Config.Username, URL_SUFFIX)
+	return url
+}
+
+// MakeNextURL finds the URL for the next page of the gists, or blank,
+// if there are no more pages.
+func MakeNextURL(resp *http.Response) string {
+	url := ""
+	for name, values := range resp.Header {
+		if name == LINK_HEADER_NAME {
+			for _, value := range values {
+				links := SplitLinks(value)
+				for _, link := range links {
+					if link.Rel == REL_NEXT_NAME {
+						url = link.URL
+					}
+				}
+			}
 		}
 	}
+	return url
 }
