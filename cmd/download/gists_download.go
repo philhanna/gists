94efd61b7fbdc575c2a7afdc4fc738c4c1aa140b
Phil Hanna <ph1204@gmail.com>
2024-01-31 14:00:59 -0500

    Fix for issue #4

--- cmd/download/gists_download.go
+++ cmd/download/gists_download.go
@@ -19,6 +19,7 @@ func init() {
 	}))
 	slog.SetDefault(logger)
 }
+
 func main() {
 
 	// Create an empty database
---
+++ cmd/fs/gists_to_files.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"bufio"
+	"database/sql"
+	"flag"
+	"fmt"
+	"gists"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const (
+	usage = `usage: gists_to_files [DBFILE] [FSROOT]
+Reads gists from a database and writes their contents to the file system
+
+positional arguments:
+  DBFILE         database file name
+  FSROOT         directory in which to write files
+
+options:
+  -f, --force    recreate directory if it exists
+`
+)
+
+var (
+	err      error
+	optForce bool
+	DBFILE   string
+	FSROOT   string
+)
+
+func main() {
+
+	// Set log style to include file name and line number
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	// Override flag.Usage so that it prints this program's usage
+	flag.Usage = func() {
+		fmt.Fprint(os.Stderr, usage)
+	}
+
+	// Parse the flags
+	flag.BoolVar(&optForce, "f", false, "recreate directory if it exists")
+	flag.BoolVar(&optForce, "force", false, "recreate directory if it exists")
+	flag.Parse()
+
+	// Verify command line arguments
+	switch flag.NArg() {
+	case 0:
+		fmt.Fprintln(os.Stderr, "No command line arguments specified")
+		fmt.Fprintln(os.Stderr, "Try --help for help")
+		return
+	case 1:
+		fmt.Fprintln(os.Stderr, "No output directory specified")
+		fmt.Fprintln(os.Stderr, "Try --help for help")
+		return
+	case 2:
+		DBFILE = flag.Arg(0)
+		if !gists.FileExists(DBFILE) {
+			log.Fatalf("%q database file does not exist", DBFILE)
+		}
+		FSROOT = flag.Arg(1)
+		_, err = os.Stat(FSROOT)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Create output directory
+	if optForce {
+		fmt.Printf("Are you sure you want to delete directory %s? (y/N) ", FSROOT)
+		reader := bufio.NewReader(os.Stdin)
+		yesno, _ := reader.ReadString('\n')
+		yesno = strings.TrimSpace(yesno)
+		switch yesno {
+		case "Y", "y":
+			err = os.RemoveAll(FSROOT)
+			if err != nil {
+				log.Fatal(err)
+			}
+			err = os.Mkdir(FSROOT, 0755)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println()
+		default:
+			// Get out
+			log.Fatalf("Exit, not deleting %s", FSROOT)
+		}
+	}
+
+	// Open the database for reading
+	log.Printf("Opening %s for input", DBFILE)
+	db, err := sql.Open("sqlite3", DBFILE)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Read each gist file from database
+	sql := `
+SELECT		f.id, f.filename, g.description, g.created_at, f.language, f.size, f.contents
+FROM		gistfiles f
+LEFT JOIN	gists g
+ON			f.id = g.id
+ORDER BY	g.created_at
+;
+`
+	rows, err := db.Query(sql)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var (
+		id          string
+		filename    string
+		description string
+		created_at  string
+		language    string
+		size        int
+		contents    []byte
+	)
+
+	for rows.Next() {
+
+		// Read a gist file record
+		err = rows.Scan(&id, &filename, &description, &created_at, &language, &size, &contents)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Create a directory to contain it
+		dirPath := filepath.Join(FSROOT, created_at)
+		err = os.Mkdir(dirPath, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Write the file contents to the directory
+		gistFileName := filepath.Join(dirPath, filename)
+		log.Printf("Writing %q to %q\n", filename, dirPath)
+		err = os.WriteFile(gistFileName, contents, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Write the metadata to the directory in JSON format
+		mdFileName := filepath.Join(dirPath, "metadata.json")
+		fp, err := os.Create(mdFileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer fp.Close()
+		fmt.Fprintf(fp, "{\n")
+		fmt.Fprintf(fp, "  %q : %q,\n", "ID", id)
+		fmt.Fprintf(fp, "  %q : %q,\n", "Description", description)
+		fmt.Fprintf(fp, "  %q : %q,\n", "Language", language)
+		fmt.Fprintf(fp, "  %q : %d\n", "Size", size)
+		fmt.Fprintf(fp, "}\n")
+	}
+}
